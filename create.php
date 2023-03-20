2399f2136d94c9c0b64da2a3e097eff59b9f4f75
Yassounevica99 <chlabbou@gmail.com>
2023-03-20 19:30:24 +0100

    php +  db

---
+++ create.php
@@ -0,0 +1,23 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "market";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO product ( name, price, quantity) VALUES ('riz', 4000,20);";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
